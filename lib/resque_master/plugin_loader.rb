fc920c1f7e56e5f1ef6755c4eabdc5e3747b2b25
Hayden Ball <hayden@haydenball.me.uk>
2015-10-07 22:09:21 +0100

    There may not be any config anymore

--- lib/resque_master/plugin_loader.rb
+++ lib/resque_master/plugin_loader.rb
@@ -3,6 +3,8 @@ require 'yaml'
 module ResqueMaster
   class PluginLoader
     def self.load
+      return unless ResqueMaster.config && ResqueMaster.config['plugins']
+
       ResqueMaster.config['plugins'].each do |plugin|
         require plugin
       end
